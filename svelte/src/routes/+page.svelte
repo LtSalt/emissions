9a41aec21655b24e4f966259a3bf7698c1fe9597
David Will <64697174+LtSalt@users.noreply.github.com>
2023-08-08 23:28:18 +0200

    Stacked area chart

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,24 +1,34 @@
 <script>
 	import '../app.css';
-	import { scaleLinear, max, line, axisBottom, axisLeft, select, format } from 'd3';
+	import {
+		scaleLinear,
+		max,
+		line,
+		axisBottom,
+		axisLeft,
+		select,
+		format,
+		stack,
+		area,
+		scaleOrdinal
+	} from 'd3';
 
+	// import data
 	export let data;
 	let { emissions } = data;
 
+	// define dimensions
 	let width = 0;
 	let height = 0;
 	let margins = { top: 20, right: 20, bottom: 40, left: 40 };
 	$: innerWidth = width - margins.left - margins.right;
 	$: innerHeight = height - margins.top - margins.bottom;
 
+	// define scales
 	$: xScale = scaleLinear([1990, 2022], [0, innerWidth]);
 	$: yScale = scaleLinear([0, max(emissions.map((d) => d.total))], [innerHeight, 0]);
 
-	$: d = line(
-		(emissions) => xScale(emissions.year),
-		(emissions) => yScale(emissions.total)
-	)(emissions);
-
+	// define and draw axes
 	let xAxis;
 	let yAxis;
 
@@ -26,6 +36,21 @@
 		select(xAxis).call(axisBottom(xScale).tickFormat(format('')));
 		select(yAxis).call(axisLeft(yScale));
 	}
+
+	// create stacks & draw areas
+	const sectors = ['energy', 'transport', 'buildings', 'industry', 'agriculture', 'waste'];
+	const series = stack().keys(sectors)(emissions);
+
+	$: areaGenerator = area() // reactive declarations (as the axes) are initilized at last
+		.x((d) => xScale(d.data.year))
+		.y0((d) => yScale(d[0]))
+		.y1((d) => yScale(d[1]));
+
+	$: stacks = series.map((sector) => areaGenerator(sector));
+
+	// color scale
+	const colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33'];
+	const colorScale = scaleOrdinal(sectors, colors);
 </script>
 
 <div id="content" class="min-h-screen grid grid-rows-[auto_1fr] gap-4">
@@ -39,9 +64,8 @@
 		<div class="aspect-square" bind:clientWidth={width} bind:clientHeight={height}>
 			<svg width="100%" height="100%">
 				<g style="transform: translate({margins.left}px, {margins.top}px);">
-					<path {d} stroke="black" stroke-width="1" fill="none" />
-					{#each emissions as d}
-						<circle r="2" cx={xScale(d.year)} cy={yScale(d.total)} />
+					{#each series as s, i}
+						<path d={stacks[i]} stroke="black" stroke-width="1" fill={colorScale(s.key)} />
 					{/each}
 					<g bind:this={xAxis} style="transform: translate(0, {innerHeight}px);" />
 					<g bind:this={yAxis} />
