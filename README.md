3a8ba8e0aed799c8897c765555fdde4211be4eb5
David Will <64697174+LtSalt@users.noreply.github.com>
2023-08-08 10:05:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+Personal project aimed at practicing Svelte and React in comparison.
+
+Data comes from the [Umweltbundesamt](https://www.umweltbundesamt.de/daten/umweltindikatoren/indikator-emission-von-treibhausgasen#die-wichtigsten-fakten)
+
+
+
+
---
+++ scripts/prep.R
@@ -0,0 +1,49 @@
+################################################################################
+# CO2 EMISSIONS IN GERMANY
+#
+# R Prep
+################################################################################
+
+
+# Dependencies ------------------------------------------------------------
+
+if(!"pacman" %in% installed.packages()) install.packages("pacman")
+pacman::p_load(here, tidyverse, readxl, janitor, jsonlite)
+
+
+# Import ------------------------------------------------------------------
+
+emissions_raw <- read_excel(here("data", "emissions.xlsx"), 
+                        sheet = "Daten", 
+                        skip = 13)
+
+
+# Cleaning ----------------------------------------------------------------
+
+emissions <- emissions_raw %>% 
+  slice(-c(1, 2)) %>% 
+  select(year = 1, 
+         `Energy Industry`, 
+         Transport, 
+         Buildings, 
+         Industry, 
+         Agriculture, 
+         `Waste and Waste Water`, 
+         `Greenhouse gas emissions total...15`) %>% 
+  clean_names() %>% 
+  mutate(across(everything(), as.numeric),
+         year = row_number() + 1989) %>% 
+  filter(year <= 2022) %>% 
+  rename(energy = energy_industry, 
+         waste = waste_and_waste_water,
+         total = greenhouse_gas_emissions_total_15)
+
+
+# Export ------------------------------------------------------------------
+
+write_excel_csv(toJSON(emissions),
+                here())
+
+  
+  
+
